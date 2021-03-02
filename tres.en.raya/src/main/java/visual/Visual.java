bf8a647ebb56d3266a80826bd6d835ec06412bd7
Sergio <sergio_castilla_13@hotmail.com>
2021-03-02 17:30:56 +0100

    fix diff

--- tres.en.raya/src/main/java/visual/Visual.java
+++ tres.en.raya/src/main/java/visual/Visual.java
@@ -2,14 +2,9 @@ package visual;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-<<<<<<< HEAD
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-=======
+
 import javax.swing.JButton;
 
->>>>>>> branch 'dev1' of https://github.com/Boguepo/Tres-en-raya.git
 
 public class Visual extends JFrame {
 
@@ -27,25 +22,12 @@ public class Visual extends JFrame {
 		setBounds(100, 100, 1042, 607);
 		
 		contentPane = new JPanel();
-<<<<<<< HEAD
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-=======
 		
 		contentPane.setLayout(null);
->>>>>>> branch 'dev1' of https://github.com/Boguepo/Tres-en-raya.git
 		setContentPane(contentPane);
-<<<<<<< HEAD
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Tres en raya");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(155, 11, 103, 23);
-		contentPane.add(lblNewLabel);
-=======
 		
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.setBounds(39, 39, 120, 120);
 		contentPane.add(btnNewButton);
->>>>>>> branch 'dev1' of https://github.com/Boguepo/Tres-en-raya.git
 	}
 }
